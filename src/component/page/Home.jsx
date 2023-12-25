b98176f586faf58cae9836fa69b2f0e0199a2f6f
YoonSoo Kim <rabbit7183@gmail.com>
2023-12-25 17:50:17 +0900

    ref: 브랜치 업데이트 확인

--- src/component/page/Home.jsx
+++ src/component/page/Home.jsx
@@ -1,19 +1,19 @@
 /*
 용도: 메인 홈 페이지
 담당자: 김윤수
-사용법: 
-기타: 
+사용법: 메인 홈 페이지로 사용
+기타: 브랜치 잘 올라가는지 확인합니다
 */
 import MenuBar from "../layout/MenuBar";
 import Header from "../layout/Header";
 
 const Home = () => {
-    return (
-      <div>
-        <Header headerType={"home"}></Header>
-        <MenuBar></MenuBar>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <Header headerType={"home"}></Header>
+      <MenuBar></MenuBar>
+    </div>
+  );
+};
 
 export default Home;
